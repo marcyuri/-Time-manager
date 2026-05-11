7104b7b101f1aeb9d7edb4df1539b14e6c07441e
YuriMarc <tagakoum@gmail.com>
2026-05-11 02:32:58 +0100

    texte valide 2

--- client/src/components/schedule/TaskForm.jsx
+++ client/src/components/schedule/TaskForm.jsx
@@ -456,7 +456,7 @@ export function TaskForm({
             className="btn--primary"
             type="submit"
           >
-            To Validate
+            Valider
           </Button>
 
         </div>
